813c86afa16c97cd6c041feff1c3317d75c37ef6
suvi321 <115531950+suvi321@users.noreply.github.com>
2025-07-25 23:15:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
-  runApp(QuranScienceExplorer());
+  runApp(QuranScienceExplorerApp());
 }
 
-class QuranScienceExplorer extends StatelessWidget {
+class QuranScienceExplorerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,9 +26,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => HomePage()),
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
       );
     });
   }
@@ -38,51 +39,10 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.auto_stories_rounded, size: 80, color: Colors.greenAccent),
-            SizedBox(height: 20),
-            Text(
-              'Quran Science Explorer',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.greenAccent),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Powered by Ak\'s-Yasir',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quran Science Explorer'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('🌌 Explore Quran & Science'),
-            SizedBox(height: 20),
-            Text(
-              'Ak\'s-Yasir',
-              style: TextStyle(fontSize: 18, color: Colors.greenAccent),
-            ),
-            SizedBox(height: 40),
-            Text(
-              'Crafted in India',
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-            )
-          ],
+        child: Image.asset(
+          'assets/icon/logo1.png',
+          width: 150,
+          height: 150,
         ),
       ),
     );
